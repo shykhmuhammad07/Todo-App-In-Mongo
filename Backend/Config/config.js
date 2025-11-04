86bbb1feaa704757200c3e6447b1116dd70311b5
shykhmuhammad07 <muhammadsheikh880@gmail.com>
2025-11-04 16:18:00 +0500

    done

--- Backend/Config/config.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import "dotenv/config";
-
-mongoose.connect(process.env.DB_URL)
-
-export default mongoose;
--- Backend/Config/model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const TodoSchema = new mongoose.Schema({
-  text: String,
-  number: Number
-});
-
-const Todo = mongoose.model("Todo", TodoSchema);
-export default Todo;
